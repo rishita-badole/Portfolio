9e8325edabb0dac1877e7559c6897993dc422344
Rishita <rishitabadole@gmail.com>
2025-07-08 00:03:21 +0530

    Initial commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,52 +1,33 @@
 import React from "react";
 import Particle from "../Particle";
-import pdf from "src\Assets\Rishita Badole - Resume.pdf"; // Updated PDF path
+import resumePDF from "../../Assets/Rishita_Badole_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
-    <div>
-      <Container fluid className="resume-section">
-        <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
+    <div className="resume-section">
+      <Particle />
+      <div className="resume-container">
+        <h1 className="project-heading">
+          My <strong className="purple">Resume</strong>
+        </h1>
+        <p style={{ color: "white" }}>Download or view my resume here:</p>
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
+        <div style={{ margin: "20px 0" }}>
+          <a href={resumePDF} download className="btn btn-primary">
+            <AiOutlineDownload style={{ marginRight: "10px" }} />
+            Download Resume (PDF)
+          </a>
+        </div>
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-      </Container>
+        <iframe
+          src={resumePDF}
+          title="Resume PDF"
+          width="100%"
+          height="600px"
+          style={{ border: "none" }}
+        />
+      </div>
     </div>
   );
 }
